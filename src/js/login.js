2104ab4ba8b60bd6798bc0e7c953522c895e2c5d
Annika Gadman <annika.gadman@gmail.com>
2025-05-08 13:35:11 -0500

    ändrat login till email

--- src/js/login.js
+++ src/js/login.js
@@ -1,14 +1,14 @@
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
     e.preventDefault();
   
-    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
   
     try {
       const res = await fetch('http://localhost:3000/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ email, password }),
       });
   
       const result = await res.json();
