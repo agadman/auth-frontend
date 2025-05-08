0f21f8c50bdd92a87b0e5caccdbba81ddede6cb4
Annika Gadman <annika.gadman@gmail.com>
2025-05-08 14:31:23 -0500

    fick göra en vite config för att hitta alla html filer i prod

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        protected: resolve(__dirname, 'protected.html'),
+      }
+    }
+  }
+})
